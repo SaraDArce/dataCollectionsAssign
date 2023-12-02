987cd3580b9c89e1f053f5849ce0e4a2d7581706
Sara D'Arce <saradarce@gmail.com>
2023-12-01 21:18:46 -0500

    Part 2

--- datacoll.js
+++ datacoll.js
@@ -5,10 +5,20 @@ const csvString = `ID,Name,Occupation,Age
 57,Bob,Fry Cook,19
 63,Blaine,Quiz Master,58
 98,Bill,Doctor’s Assistant,26`;
-let rows = csvString.split(“\n”);
+let rows = csvString.split("\n");
 for (let i = 0; i < rows.length; i++) {
   // Split the current row into individual elements (id, name, occupation, age)
   // based on the comma delimiter and assign them to respective variables.
   let [cell1, cell2, cell3, cell4] = rows[i].split(",");
   //console.log(cell1, cell2, cell3, cell4);
 }
+
+//Part2
+let lines = csvString.split("\n");
+let colNum = lines[0].split(",").length;
+let twoD = [];
+for (let line of lines) {
+  let row = line.split(",");
+  if (row.length === colNum) twoD.push(row);
+}
+console.log(twoD);
