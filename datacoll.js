b0dc002d16775efe36e7997b825054aac5c1dcea
Sara D'Arce <saradarce@gmail.com>
2023-12-01 21:58:30 -0500

    Part 4

--- datacoll.js
+++ datacoll.js
@@ -36,3 +36,42 @@ for (let i = 1; i < lines.length; i++) {
   result.push(dataObj);
 }
 console.log(result);
+
+//Part 4
+//let lines = csvString.split(‘\n’);
+//using const map1 = array1.map((x) => x * 2);
+let header1 = lines[0].split(",").map((header1) => header1.toLowerCase());
+console.log("header pre lower-case method:", header1);
+result = [];
+
+for (let i = 1; i < lines.length; i++) {
+  let values = lines[i].split(",");
+  let dataObj = {};
+  for (let j = 0; j < header.length; j++) {
+    dataObj[header1[j]] = values[j];
+  }
+  result.push(dataObj);
+  console.log("The result asked by Alaa:", result);
+  console.log(result.pop());
+  console.log("After pop:", result);
+  console.log(
+    result.splice(1, 0, {
+      id: "48",
+      name: "Barry",
+      occupation: "Runner",
+      age: "25",
+    })
+  );
+  console.log("After splice:", result);
+  console.log(
+    result.push({ id: "7", name: "Bilbo", occupation: "None", age: "111" })
+  );
+  console.log("After push:", result);
+  let totalAge = 0;
+  for (let i = 0; i < result.length; i++) {
+    totalAge += parseInt(result[i].age);
+    console.log("total ages:", totalAge, result[i].age);
+  }
+  let avgAge = totalAge / result.length;
+  console.log("avgAge:", Math.round(avgAge));
+}
